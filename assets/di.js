620c39b4938df060feae440a0e03a1cfee40b8dd
Can İbrahim Koç <info@canibrahim.com>
2019-04-14 02:46:59 +0300

    v1.0.1 revised

---
+++ assets/di.js
@@ -0,0 +1,14 @@
+	$(document).ready(function(){
+		$.ajax({
+			url:"assets/main.css",
+            dataType:"script",
+            cache: true,
+			success:function(data){
+				$("body").append("<div id='double-important'><style>" + data + "</style></div>");
+			}
+        });
+        var hardImport = " !important;"  
+
+	});
+
+    
