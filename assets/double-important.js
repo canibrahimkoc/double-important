15fe1209fae38ed99cff9664f217118ae6883351
Can İbrahim Koç <info@canibrahim.com>
2019-04-14 22:38:25 +0300

    v1.0.3 revised

--- assets/double-important.js
+++ assets/double-important.js
@@ -4,8 +4,9 @@
             dataType:"script",
             cache: true,
 			success:function(data){
-				$("body").append("<div id='double-important'><style>" + data + "</style></div>");
+				var $importData = data;
+				// $("body").append("<div id='double-important'><style>" + $importData + "</style></div>");
+				// $importData
 			}
-        });
-        var hardImport = " !important;"  
+		});
 	});
